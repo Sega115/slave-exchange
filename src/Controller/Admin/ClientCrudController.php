7d6c7a752c708eb9dc740905fd580b91d094ad7d
starser9e <starser9e@yandex.ru>
2021-05-29 12:02:12 +0300

    refactor clientGrud

--- src/Controller/Admin/ClientCrudController.php
+++ src/Controller/Admin/ClientCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Client;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ClientCrudController extends AbstractCrudController
@@ -10,5 +11,10 @@ class ClientCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Client::class;
+    }
+
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        return;
     }
 }
